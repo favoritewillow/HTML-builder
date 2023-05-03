27124bc52eadae1d7c56c22a477e1d68f6c3cf48
Natalie <favoritewillow@gmail.com>
2023-05-04 01:02:04 +0300

    feat: add readStream to 01-read-file (Thursday 04.05.2023 1:01pm)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,20 @@ const fs = require("fs");
 
 const { stdout } = process;
 
+const readStream = fs.createReadStream("./01-read-file/text.txt", "utf8");
+
+readStream.on("data", (chunk) => {
+  stdout.write(`Received ${chunk.length} bytes of data.`);
+});
+
+readStream.on("end", () => {
+  stdout.write("Finished reading data.");
+});
+
+readStream.on("error", (err) => {
+  stdout.write(`Error occurred: ${err}`);
+});
+
 fs.readFile("./01-read-file/text.txt", "utf8", (err, data) => {
   if (err) {
     throw err;
