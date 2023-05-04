eddc95f09ba8e5dc4103cf2d122430be12b4e287
Natalie <favoritewillow@gmail.com>
2023-05-04 22:19:15 +0300

    feat: add solution 02-write-file (Thursday 04.05.2023 22:19pm)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,1 +1,42 @@
 const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout } = process;
+
+fs.writeFile(
+  path.join(__dirname, 'texts.txt'),
+  'Hello! How are you?\n',
+  (err) => {
+    if (err) throw err;
+  },
+);
+
+fs.readFile('./02-write-file/texts.txt', 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  stdout.write(data);
+});
+
+const filename = 'texts.txt';
+const filepath = path.join(__dirname, filename);
+
+fs.writeFile(filepath, '', (err) => {
+  if (err) throw err;
+
+  stdin.setEncoding('utf8');
+  stdin.on('data', (input) => {
+    if (input.trim() === 'exit') {
+      stdout.write('Thank you!\n');
+      process.exit();
+    }
+
+    fs.appendFile(filepath, input, (error) => {
+      if (error) throw err;
+
+      stdout.write(`"${input.trim()}" успешно записано в ${filename}\n`);
+      stdout.write('Введите текст:\n');
+    });
+  });
+});
