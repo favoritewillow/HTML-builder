2cb5d8abecc70d39e8ae4635762c8ae6e53cce38
Natalie <favoritewillow@gmail.com>
2023-05-05 00:33:11 +0300

    feat: add solution 04-copy-directory (Friday 05.05.2023 0:33pm)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+const { stdout } = process;
+
+const secretFolder = './04-copy-directory/files';
+
+fs.mkdir(
+  path.join(__dirname, 'files-copy'),
+  { recursive: true },
+  (mkdirErr) => {
+    if (mkdirErr) throw mkdirErr;
+    stdout.write('Папка была создана');
+
+    fs.readdir(secretFolder, (err, files) => {
+      if (err) throw err;
+
+      files.forEach((file) => {
+        const filePath = path.join(secretFolder, file);
+        const copyFilePath = path.join(__dirname, 'files-copy', file);
+
+        fs.copyFile(filePath, copyFilePath, (pathErr) => {
+          if (pathErr) throw pathErr;
+          stdout.write(`${file} был скопирован`);
+        });
+      });
+    });
+  },
+);
